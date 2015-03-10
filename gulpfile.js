28753c16ab97d0f03559ad80a0c0faa9cc846140
BafS <fabacrans@hotmail.com>
2015-03-10 18:36:05 +0100

    correct gulp sources

--- gulpfile.js
+++ gulpfile.js
@@ -9,26 +9,26 @@ var jshint = require('gulp-jshint');
 gulp.task('default', ['styles', 'scripts', 'fonts'], function() { });
 
 gulp.task('styles', function () {
-	return gulp.src('./less/**/*.less')
+	return gulp.src('./src/less/**/*.less')
 		.pipe(less())
 		.pipe(minifyCSS())
 		.pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('scripts', function () {
-	return gulp.src('./js/**/*.js')
+	return gulp.src('./src/js/**/*.js')
 		.pipe(babel())
 		.pipe(minify())
 		.pipe(gulp.dest('./dist/js'));
 });
 
 gulp.task('fonts', function () {
-	return gulp.src('./fonts/**/*')
+	return gulp.src('./src/fonts/**/*')
 		.pipe(gulp.dest('./dist/fonts'));
 });
 
 gulp.task('jshint', function() {
-  gulp.src('./js/**/*.js')
+  gulp.src('./src/js/**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
