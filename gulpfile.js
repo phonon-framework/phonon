8bdb92c603d02fc15ec3ff1f091fd12b78575efd
Qathom <tony9295@gmail.com>
2015-03-10 22:04:38 +0100

    minified css added

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,26 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
-var minifyCSS = require('gulp-minify-css');
+var cssmin = require('gulp-cssmin');
 var minify = require('gulp-minify');
 var babel = require('gulp-babel');
+var rename = rename = require('gulp-rename');
 // var watch = require('gulp-watch');
 var jshint = require('gulp-jshint');
 
-gulp.task('default', ['styles', 'scripts', 'fonts'], function() { });
+gulp.task('default', ['styles', 'min-styles', 'scripts', 'fonts'], function() { });
 
 gulp.task('styles', function () {
 	return gulp.src('./src/less/**/*.less')
 		.pipe(less())
-		.pipe(minifyCSS())
+		.pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('min-styles', function () {
+	return gulp.src('./dist/css/**/*.css')
+		.pipe(cssmin().on('error', function(err) {
+			console.log(err);
+		}))
+		.pipe(rename({suffix: '-min'}))
 		.pipe(gulp.dest('./dist/css'));
 });
 
@@ -19,7 +28,7 @@ gulp.task('scripts', function () {
 	return gulp.src('./src/js/**/*.js')
 		.pipe(babel())
 		.pipe(minify())
-		.pipe(gulp.dest('./dist/js'));
+		.pipe(gulp.dest('./dist/js'));	
 });
 
 gulp.task('fonts', function () {
