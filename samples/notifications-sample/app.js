a41148e91f46cbd32c7f7d78a944297645a8d19c
Qathom <tony9295@gmail.com>
2015-04-04 14:47:30 +0200

    timeout example added

--- samples/notifications-sample/app.js
+++ samples/notifications-sample/app.js
@@ -1,21 +1,23 @@
 'use strict';
 
-Phonon.Notification(document.querySelector('#welcome')).show();
+window.addEventListener('load', function () {
+
+	Phonon.Notification(document.querySelector('#welcome')).show();
+
+	var myNotif = document.querySelector('#my-notif');
+
+	myNotif.addEventListener('phonon-notif:opened', function (evt) {
+	    console.log('opened!');
+	    console.log(evt.target);
+	});
+	myNotif.addEventListener('phonon-notif:hidden', function (evt) {
+	    console.log('hidden!');
+	    console.log(evt.target);
+	});
 
 
-var myNotif = document.querySelector('#my-notif');
-
-myNotif.addEventListener('phonon-notif:opened', function (evt) {
-    console.log('opened!');
-    console.log(evt.target);
-});
-myNotif.addEventListener('phonon-notif:hidden', function (evt) {
-    console.log('hidden!');
-    console.log(evt.target);
-});
-
-
-document.querySelector('#show-notif-btn').addEventListener('click', function (evt) {
-	evt.preventDefault();
-	Phonon.Notification(myNotif).show();
+	document.querySelector('#show-notif-btn').addEventListener('click', function (evt) {
+		evt.preventDefault();
+		Phonon.Notification(myNotif).show();
+	});
 });
