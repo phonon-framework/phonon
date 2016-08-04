3bc751157203d1d7ae6adfc89ef1fe32fa490491
Qathom <tony9295@gmail.com>
2016-08-05 01:59:26 +0200

    Release 1.2.9

--- dist/js/phonon.js
+++ dist/js/phonon.js
@@ -3931,11 +3931,8 @@ phonon.tagManager = (function () {
     previousPopover.style.visibility = 'hidden';
     if(previousPopover.getAttribute('data-virtual') === 'true') {
         // remove from DOM
-        window.setTimeout(function () {
-            document.body.removeChild(document.body.lastChild);
-            console.log(document.body)
-            previousPopover = null;
-        }, 1000);
+        document.body.removeChild(previousPopover);
+        previousPopover = null;
     }
   }
 
