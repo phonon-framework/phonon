ad8990177fee292156342009446a6dd92426fb80
Qathom <tony9295@gmail.com>
2017-11-23 20:25:22 +0100

    Clean code

--- src/js/core/platform/index.js
+++ src/js/core/platform/index.js
@@ -1,7 +1,8 @@
-export function platform () {
-  /* Use of platform.js
-   * https://github.com/bestiejs/platform.js
-   * License: https://github.com/bestiejs/platform.js/blob/master/LICENSE
-   */
-  return require('platform')
-}
+/*
+ * Use of platform.js
+ * https://github.com/bestiejs/platform.js
+ * License: https://github.com/bestiejs/platform.js/blob/master/LICENSE
+ */
+import platform from 'platform'
+
+export default platform
