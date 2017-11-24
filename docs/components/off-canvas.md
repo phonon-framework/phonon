4d5b847b855e0f9a6c7f9467bb078a71554a21d1
Qathom <tony9295@gmail.com>
2017-11-24 23:56:18 +0100

    Added OffCanvas component

---
+++ docs/components/off-canvas.md
@@ -0,0 +1,116 @@
+# Off Canvas
+
+## Introduction
+
+[WIP]
+
+## Markup
+
+```html
+<div class="off-canvas" id="exampleOffCanvas" role="navigation" aria-hidden="true" aria-labelledby="exampleOffCanvasTitle">
+  <div class="off-canvas-inner">
+    <div class="off-canvas-content">
+      <div class="off-canvas-header">
+        <h1 class="off-canvas-title">Title</h1>
+        <button type="button" class="close" data-dismiss="off-canvas" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="off-canvas-body">
+        <p>Content</p>
+      </div>
+      <div class="off-canvas-footer">
+        <p>Footer</p>
+      </div>
+    </div>
+  </div>
+</div>
+```
+
+Then, you can work with it by using the correct `element` property.
+
+```js
+const offCanvas = phonon.offCanvas({
+  element: '#myOffCanvas'
+})
+
+offCanvas.show()
+```
+
+## Options
+
+- cancelable
+
+## Methods
+
+### show()
+
+Any off canvas can be shown with JavaScript. For this, we call the `show()` method:
+
+```js
+offCanvas.show()
+```
+
+
+### hide()
+
+Any off canvas can be hidden with JavaScript, not only by clicking on its buttons. For this, we call the `hide()` method:
+
+```js
+offCanvas.hide()
+```
+
+
+## Events
+
+It may be useful to use the events that affect your off-canvas.
+To do this, you can use object and DOM events.
+
+
+|     Event Type     |     Description      |
+|--------------------|----------------------|
+|  show    |   This event fires immediately when the <code>show</code> instance method is called. If caused by a click, the clicked element is available as the <code>relatedTarget</code> property of the event.   |
+|  shown   |  This event is fired when the off-canvas has been made visible to the user (will wait for CSS transitions to complete). If caused by a click, the clicked element is available as the <code>relatedTarget</code> property of the event.    |
+|  hide    |    This event is fired immediately when the <code>hide</code> instance method has been called.   |
+|  hidden  |   This event is fired when the off-canvas has finished being hidden from the user (will wait for CSS transitions to complete).    |
+
+
+### Object Events
+
+```js
+phonon.offCanvas({
+  element: '#exampleOffCanvas',
+  show: () => { // or onShow
+    console.log('It works!')
+  },
+  shown: () => { // or onShown
+    console.log('It works!')
+  },
+  hide: () => { // or onHide
+    console.log('It works!')
+  },
+  hidden: () => { // or onHidden
+    console.log('It works!')
+  }
+})
+```
+
+### DOM Events
+
+```js
+document.querySelector('.off-canvas').addEventListener('show.ph.off-canvas', () => {
+  console.log('It works!')
+})
+
+document.querySelector('.off-canvas').addEventListener('shown.ph.off-canvas', () => {
+  console.log('It works!')
+})
+
+document.querySelector('.off-canvas').addEventListener('hide.ph.off-canvas', () => {
+  console.log('It works!')
+})
+
+document.querySelector('.off-canvas').addEventListener('hidden.ph.off-canvas', () => {
+  console.log('It works!')
+})
+```
---
+++ src/js/components/off-canvas/index.js
@@ -0,0 +1,255 @@
+/**
+ * --------------------------------------------------------------------------
+ * Licensed under MIT (https://github.com/quark-dev/Phonon-Framework/blob/master/LICENSE)
+ * --------------------------------------------------------------------------
+ */
+import Event from '../../core/events'
+import Component from '../component'
+
+const OffCanvas = (() => {
+  /**
+   * ------------------------------------------------------------------------
+   * Constants
+   * ------------------------------------------------------------------------
+   */
+
+  const NAME = 'off-canvas'
+  const VERSION = '2.0.0'
+  const BACKDROP_SELECTOR = 'off-canvas-backdrop'
+  const DEFAULT_PROPERTIES = {
+    element: null,
+    aside: {
+      md: false,
+      lg: false,
+    }
+  }
+
+  /**
+   * ------------------------------------------------------------------------
+   * Class Definition
+   * ------------------------------------------------------------------------
+   */
+
+  class OffCanvas extends Component {
+
+    constructor(options = {}) {
+      super(NAME, VERSION, DEFAULT_PROPERTIES, options, false)
+
+      this.useBackdrop = false
+
+      this.currentWidth = null
+
+      this.animate = true
+
+      const sm = { name: 'sm', media: window.matchMedia('(min-width: 1px)') }
+      const md = { name: 'md', media: window.matchMedia('(min-width: 768px)') }
+      const lg = { name: 'lg', media: window.matchMedia('(min-width: 992px)') }
+      const xl = { name: 'xl', media: window.matchMedia('(min-width: 1200px)') }
+
+      const sizes = [sm, md, lg, xl].reverse()
+
+      const checkWidth = () => {
+        if (!('matchMedia' in window)) {
+          return
+        }
+
+        sizes.every((size) => {
+          const match = size.media.media.match(/[a-z]?-width:\s?([0-9]+)/)
+
+          if (match) {
+            if (size.media.matches) {
+              if (this.currentWidth !== size.name) {
+                this.setAside(size.name)
+              }
+              this.currentWidth = size.name
+              return false
+            }
+          }
+
+          return true
+        })
+      }
+
+      setTimeout(checkWidth, 1)
+
+      window.addEventListener('resize', checkWidth, false)      
+    }
+
+    closable() {
+      return this.options.aside[this.currentWidth] !== true
+    }
+
+    setAside(name) {
+      const content = document.body
+
+      if (this.options.aside[name] === true) {
+        if (!content.classList.contains('off-canvas-aside')) {
+          content.classList.add('off-canvas-aside')
+        }
+
+        this.useBackdrop = false
+
+        // avoid animation by setting animate to false
+        this.animate = false
+        this.show()
+        // remove previous backdrop
+        this.removeBackdrop()
+      } else {
+        if (content.classList.contains('off-canvas-aside')) {
+          content.classList.remove('off-canvas-aside')
+        }
+
+        this.hide()
+        this.useBackdrop = true
+        this.animate = true
+      }
+    }
+
+    onElementEvent(event) {
+      if (event.type === 'keyup' && event.keyCode !== 27 && event.keyCode !== 13) {
+        return
+      }
+
+      if (!this.closable()) {
+        return
+      }
+
+      // hide the off-canvas
+      this.hide()
+    }
+
+    show() {
+      if (this.options.element.classList.contains('show')) {
+        console.error(`${NAME}. The off-canvas is already visible.`)
+        return false
+      }
+
+      // add a timeout so that the CSS animation works
+      setTimeout(() => {
+        this.triggerEvent(Event.SHOW)
+
+        const onShown = () => {
+          this.triggerEvent(Event.SHOWN)
+
+          if (this.animate) {
+            this.options.element.removeEventListener(Event.TRANSITION_END, onShown)
+            this.options.element.classList.remove('animate')
+          }
+        }
+
+        if (this.useBackdrop) {
+          this.createBackdrop()
+        }
+
+
+        if (this.animate) {
+          this.options.element.addEventListener(Event.TRANSITION_END, onShown)        
+          this.options.element.classList.add('animate')
+        } else {
+          // directly trigger the onShown
+          onShown()
+        }
+
+        this.options.element.classList.add('show')        
+
+        // attach event
+        this.attachEvents()
+      }, 1)
+
+      return true
+    }
+
+    hide() {
+      if (!this.options.element.classList.contains('show')) {
+        console.error(`${NAME}. The off-canvas is not visible.`)
+        return false
+      }
+
+      this.triggerEvent(Event.HIDE)
+
+      this.detachEvents()
+
+      if (this.animate) {
+        this.options.element.classList.add('animate')
+      }
+
+      this.options.element.classList.remove('show')
+
+      if (this.useBackdrop) {
+        const backdrop = this.getBackdrop()
+
+        const onHidden = () => {
+          if (this.animate) {
+            this.options.element.classList.remove('animate')
+          }
+
+          backdrop.removeEventListener(Event.TRANSITION_END, onHidden)
+          this.triggerEvent(Event.HIDDEN)        
+          this.removeBackdrop()
+        }
+
+        backdrop.addEventListener(Event.TRANSITION_END, onHidden)
+        backdrop.classList.add('fadeout')
+      }
+
+      return true
+    }
+
+    createBackdrop() {
+      const backdrop = document.createElement('div')
+      backdrop.setAttribute('data-id', this.id)
+      backdrop.classList.add(BACKDROP_SELECTOR)
+
+      document.body.appendChild(backdrop)
+    }
+
+    getBackdrop() {
+      return document.querySelector(`.${BACKDROP_SELECTOR}[data-id="${this.id}"]`)
+    }
+
+    removeBackdrop() {
+      const backdrop = this.getBackdrop()
+      if (backdrop) {
+        document.body.removeChild(backdrop)
+      }
+    }
+
+    attachEvents() {
+      const dismissButtons = this.options.element.querySelectorAll('[data-dismiss]')
+
+      if (dismissButtons) {
+        dismissButtons.forEach(button => this.registerElement({ target: button, event: 'click' }))
+      }
+
+      if (this.useBackdrop) {
+        const backdrop = this.getBackdrop()      
+        this.registerElement({ target: backdrop, event: Event.START })
+      }
+
+      this.registerElement({ target: document, event: 'keyup' })
+    }
+
+    detachEvents() {
+      const dismissButtons = this.options.element.querySelectorAll('[data-dismiss]')
+
+      if (dismissButtons) {
+        dismissButtons.forEach(button => this.unregisterElement({ target: button, event: 'click' }))
+      }
+
+      if (this.useBackdrop) {
+        const backdrop = this.getBackdrop()
+        this.unregisterElement({ target: backdrop, event: Event.START })
+      }
+
+      this.unregisterElement({ target: document, event: 'keyup' })
+    }
+
+    static _DOMInterface(options) {
+      return super._DOMInterface(OffCanvas, options)
+    }
+  }
+
+  return OffCanvas
+})()
+
+export default OffCanvas
