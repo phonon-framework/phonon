b40518f583a49f3444810ee92b29af310bf734a9
Qathom <tony9295@gmail.com>
2017-11-25 13:43:57 +0100

    Added object events

--- docs/core/network.md
+++ docs/core/network.md
@@ -1,6 +1,11 @@
 # Network
 
+## Introduction
 Network is a simple module to know the status of the network, i. e. whether the user has a working connection or not.
+
+## Events
+
+### DOM Events
 
 ```js
 window.addEventListener('online.ph.network', function (event) {
@@ -11,5 +16,18 @@ window.addEventListener('online.ph.network', function (event) {
 window.addEventListener('offline.ph.network', function (event) {
   console.log('offline')
   console.log(event.detail.date)
+})
+```
+
+### Object Events
+
+```js
+const network = phonon.network({
+  online: (event) => {
+    console.log(`online ${event.date}`)
+  },
+  offline: (event) => {
+    console.log(`offline ${event.date}`)
+  }
 })
 ```
--- src/js/core/network/index.js
+++ src/js/core/network/index.js
@@ -5,10 +5,19 @@
  */
 
 import Event from '../events'
+import Component from '../../components/component'
 import { dispatchWinDocEvent } from '../events/dispatch'
 
 const Network = (() => {
+  /**
+   * ------------------------------------------------------------------------
+   * Constants
+   * ------------------------------------------------------------------------
+   */
+
   const NAME = 'network'
+  const VERSION = '2.0.0'
+  const DEFAULT_PROPERTIES = {}
 
   window.addEventListener('online', () => {
     dispatchWinDocEvent(Event.NETWORK_ONLINE, NAME, { date: new Date() })
@@ -17,6 +26,36 @@ const Network = (() => {
   window.addEventListener('offline', () => {
     dispatchWinDocEvent(Event.NETWORK_OFFLINE, NAME, { date: new Date() })
   })
+
+  /**
+   * ------------------------------------------------------------------------
+   * Class Definition
+   * ------------------------------------------------------------------------
+   */
+
+  class Network extends Component {
+
+    constructor(options = {}) {
+      super(NAME, VERSION, DEFAULT_PROPERTIES, options, true)
+      this.addEvents()
+    }
+
+    addEvents() {
+      window.addEventListener('online.ph.network', () => {
+        this.triggerEvent(Event.NETWORK_ONLINE, { date: new Date() }, true)
+      })
+
+      window.addEventListener('offline.ph.network', () => {
+        this.triggerEvent(Event.NETWORK_OFFLINE, { date: new Date() }, true)
+      })
+    }
+
+    static _DOMInterface(options) {
+      return super._DOMInterface(Network, options)
+    }
+  }
+
+  return Network
 })()
 
 export default Network
