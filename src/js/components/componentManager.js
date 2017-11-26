cab96677953c290f28badc4526d3c7b7a14c5e8f
Qathom <tony9295@gmail.com>
2017-11-26 22:14:45 +0100

    Added concurrency for components

--- src/js/components/componentManager.js
+++ src/js/components/componentManager.js
@@ -1,17 +1,91 @@
 
+const getAttribute = (first, second) => {
+  if (first === '') {
+    return `data-${second}`
+  }
+  return `data-${first}-${second}`
+}
+
+export function setAttributesConfig(element, obj = {}, attrs, start = '') {
+  const keys = Object.keys(obj)
+
+  keys.forEach((key) => {
+    if (start === '' && attrs.indexOf(key) === -1) {
+      // continue with next iteration
+      return
+    }
+
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      let keyStart = key
+      if (start !== '') {
+        keyStart = `${start}-${key}`
+      }
+
+      setAttributesConfig(element, obj[key], attrs, keyStart)
+      return
+    }
+
+    const attr = getAttribute(start, key)
+    element.setAttribute(attr, obj[key])
+  })
+}
+
+export function getAttributesConfig(element, obj = {}, attrs, start = '') {
+  const newObj = obj
+  const keys = Object.keys(obj)
+
+  keys.forEach((key) => {
+    if (start === '' && attrs.indexOf(key) === -1) {
+      // continue with next iteration
+      return
+    }
+
+    if (obj[key] !== null && obj[key].constructor === Object) {
+      let keyStart = key
+      if (start !== '') {
+        keyStart = `${start}-${key}`
+      }
+
+      newObj[key] = getAttributesConfig(element, obj[key], attrs, keyStart)
+      return
+    }
+
+    // update value
+    let value = newObj[key] // default value
+    const type = typeof value
+    const attr = getAttribute(start, key)
+    const attrValue = element.getAttribute(attr)
+
+    if (attrValue !== null) {
+      if (type === 'boolean') {
+        // convert string to boolean
+        value = attrValue === 'true'
+      } else if (!isNaN(attrValue)) {
+        value = parseInt(attrValue, 10)
+      } else {
+        value = attrValue
+      }
+    }
+
+    newObj[key] = value
+  })
+
+  return newObj
+}
+
 const stack = []
 
 export default {
-  addComponentToStack(component) {
+  add(component) {
     stack.push(component)
   },
-  removeComponentToStack(component) {
+  remove(component) {
     const index = stack.findIndex(c => Object.is(component, c))
     if (index > -1) {
       stack.splice(index, 1)
     }
   },
-  componentClosable(component) {
+  closable(component) {
     return stack.length === 0 || Object.is(stack[stack.length - 1], component)
   }
 }
