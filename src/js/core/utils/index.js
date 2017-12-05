1133dec5a58b8aae934b73adc0e2ca4b88fd6af9
Qathom <tony9295@gmail.com>
2017-12-05 15:21:58 +0100

    Rename util functions

--- src/js/core/utils/index.js
+++ src/js/core/utils/index.js
@@ -21,3 +21,34 @@ export function loadFile(url, fn, postData) {
 export function generateId() {
   return Math.random().toString(36).substr(2, 10)
 }
+
+export function findTargetByClass(target, parentClass) {
+  for (; target && target !== document; target = target.parentNode) {
+    if (target.classList.contains(parentClass)) {
+      return target
+    }
+  }
+
+  return null
+}
+
+
+export function findTargetById(target, parentId) {
+  for (; target && target !== document; target = target.parentNode) {
+    if (target.getAttribute('id') === parentId) {
+      return target
+    }
+  }
+
+  return null
+}
+
+export function findTargetByAttr(target, attr) {
+  for (; target && target !== document; target = target.parentNode) {
+    if (target.getAttribute(attr) !== null) {
+      return target
+    }
+  }
+
+  return null
+}
