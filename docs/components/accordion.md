468c676cde65f50c095e4d23752a7a4120284c5f
Qathom <tony9295@gmail.com>
2017-12-05 15:19:26 +0100

    Create accordion component

---
+++ docs/components/accordion.md
@@ -0,0 +1,53 @@
+# Accordion
+
+## Introduction
+
+[WIP]
+
+## Markup
+
+```html
+<div class="accordion" id="exampleAccordion" role="tablist">
+  <a class="d-block" data-toggle="accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+    Collapsible Group Item #1
+  </a>
+
+  <div id="collapseOne" class="collapse" role="tabpanel" aria-labelledby="headingOne">
+    <div class="card-body">
+      This is the content of the group item #1.
+    </div>
+  </div>
+
+  <a class="d-block" data-toggle="accordion" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+    Collapsible Group Item #2
+  </a>
+
+  <div id="collapseTwo" class="collapse" role="tabpane2" aria-labelledby="headingTwo">
+    <div class="card-body">
+      This is the content of the group item #2.
+    </div>
+  </div>
+</div>
+```
+
+## JavaScript
+
+```js
+const accordion = phonon.accordion({
+  element: '#exampleAccordion',
+})
+```
+
+## Methods
+
+### show(collapse: any)
+
+```js
+accordion.show('#myCollapse')
+```
+
+### hide(collapse: any)
+
+```js
+accordion.hide('#myCollapse')
+```
---
+++ src/js/components/accordion/index.js
@@ -0,0 +1,178 @@
+/**
+ * --------------------------------------------------------------------------
+ * Licensed under MIT (https://github.com/quark-dev/Phonon-Framework/blob/master/LICENSE)
+ * --------------------------------------------------------------------------
+ */
+import Component from '../component'
+import Collapse from '../collapse'
+import { getAttributesConfig } from '../componentManager'
+import Event from '../../core/events'
+import { findTargetByClass } from '../../core/utils'
+
+const Accordion = (() => {
+  /**
+   * ------------------------------------------------------------------------
+   * Constants
+   * ------------------------------------------------------------------------
+   */
+
+  const NAME = 'accordion'
+  const VERSION = '2.0.0'
+  const DEFAULT_PROPERTIES = {
+    element: null,
+  }
+  const DATA_ATTRS_PROPERTIES = [
+  ]
+
+  /**
+   * ------------------------------------------------------------------------
+   * Class Definition
+   * ------------------------------------------------------------------------
+   */
+
+  class Accordion extends Component {
+
+    constructor(options = {}) {
+      super(NAME, VERSION, DEFAULT_PROPERTIES, options, DATA_ATTRS_PROPERTIES, false, false)
+
+      this.collapses = []
+
+      const toggles = this.options.element.querySelectorAll(`[data-toggle="${NAME}"]`)
+      toggles.forEach((toggle) => {
+        const collapseId = toggle.getAttribute('href')
+        const collapse = document.querySelector(collapseId)
+
+        if (collapse) {
+          this.addCollapse(collapse)
+        }
+      })
+    }
+
+    onElementEvent(event) {
+      const id = event.target.getAttribute('href')
+      const element = document.querySelector(id)
+
+      this.setCollapses(element)
+    }
+
+    addCollapse(element) {
+      const collapse = new Collapse({
+        element,
+      })
+      this.collapses.push(collapse)
+
+      return collapse
+    }
+
+    getCollapse(element) {
+      let collapse = this.collapses.find(c => c.options.element.getAttribute('id') === element.getAttribute('id'))
+
+      if (!collapse) {
+        // create a new collapse
+        collapse = this.addCollapse()
+      }
+
+      return collapse
+    }
+
+    getCollapses() {
+      return this.collapses
+    }
+
+    setCollapses(showCollapse) {
+      const collapse = this.getCollapse(showCollapse)
+      this.collapses.forEach((c) => {
+        if (c.options.element.getAttribute('id') !== showCollapse.getAttribute('id')) {
+          c.hide()
+        } else {
+          collapse.toggle()
+        }
+      })
+    }
+
+    show(collapseEl) {
+      let collapse = collapseEl
+      if (typeof collapseEl === 'string') {
+        collapse = document.querySelector(collapseEl)
+      }
+
+      if (!collapse) {
+        throw new Error(`${NAME}. The collapsible ${collapseEl} is an invalid HTMLElement.`)
+      }
+
+      this.setCollapses(collapse)
+
+      return true
+    }
+
+    hide(collapseEl) {
+      let collapse = collapseEl
+      if (typeof collapseEl === 'string') {
+        collapse = document.querySelector(collapseEl)
+      }
+
+      if (!collapse) {
+        throw new Error(`${NAME}. The collapsible ${collapseEl} is an invalid HTMLElement.`)
+      }
+
+      const collapseObj = this.getCollapse(collapse)
+      return collapseObj.hide()
+    }
+
+    static _DOMInterface(options) {
+      return super._DOMInterface(Accordion, options)
+    }
+  }
+
+  /**
+   * ------------------------------------------------------------------------
+   * DOM Api implementation
+   * ------------------------------------------------------------------------
+   */
+  const components = []
+
+  const accordions = document.querySelectorAll(`.${NAME}`)
+  if (accordions) {
+    accordions.forEach((element) => {
+      const config = getAttributesConfig(element, DEFAULT_PROPERTIES, DATA_ATTRS_PROPERTIES)
+      config.element = element
+
+      components.push(Accordion._DOMInterface(config))
+    })
+  }
+
+  if (accordions) {
+    document.addEventListener('click', (event) => {
+      const dataToggleAttr = event.target.getAttribute('data-toggle')
+      if (dataToggleAttr && dataToggleAttr === NAME) {
+        const collapseId = event.target.getAttribute('data-target') || event.target.getAttribute('href')
+        const collapseEl = document.querySelector(collapseId)
+
+        const accordion = findTargetByClass(event.target, 'accordion')
+
+        if (accordion === null) {
+          return
+        }
+
+        const accordionId = accordion.getAttribute('id')
+        const component = components.find(c => c.getElement().getAttribute('id') === accordionId)
+
+        if (!component) {
+          return
+        }
+
+        // if the collapse has been added programmatically, we add it
+        const targetCollapse = component.getCollapses().find(c => c.getElement() === collapseEl)
+        if (!targetCollapse) {
+          component.addCollapse(collapseEl)
+        }
+
+        component.show(collapseId)
+      }
+    })
+  }
+
+  return Accordion
+})()
+
+export default Accordion
