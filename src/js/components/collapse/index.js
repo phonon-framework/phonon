644eab3d495392ff3f0a75fe730fa112cd970b40
Qathom <tony9295@gmail.com>
2017-12-05 15:22:59 +0100

    Improve collapse behavior

--- src/js/components/collapse/index.js
+++ src/js/components/collapse/index.js
@@ -3,6 +3,10 @@
  * Licensed under MIT (https://github.com/quark-dev/Phonon-Framework/blob/master/LICENSE)
  * --------------------------------------------------------------------------
  */
+import Component from '../component'
+import { getAttributesConfig } from '../componentManager'
+import Event from '../../core/events'
+import { findTargetByAttr } from '../../core/utils'
 
 const Collapse = (() => {
   /**
@@ -17,6 +21,9 @@ const Collapse = (() => {
     element: null,
     toggle: false,
   }
+  const DATA_ATTRS_PROPERTIES = [
+    'toggle',
+  ]
 
   /**
    * ------------------------------------------------------------------------
@@ -24,48 +31,148 @@ const Collapse = (() => {
    * ------------------------------------------------------------------------
    */
 
-  class Collapse {
+  class Collapse extends Component {
 
     constructor(options = {}) {
-      this.options = Object.assign(DEFAULT_PROPERTIES, options)
+      super(NAME, VERSION, DEFAULT_PROPERTIES, options, DATA_ATTRS_PROPERTIES, false, false)
 
-      if (typeof this.options.element === 'string') {
-        this.options.element = document.querySelector(this.options.element)
+      this.onTransition = false
+
+      // toggle directly
+      if (this.options.toggle) {
+        this.show()
       }
+    }
 
-      if (this.options.element === null) {
-        throw new Error(`${NAME}. The element is not a HTMLElement.`)
-      }
-
-      this.clickTargetHandler = (event) => {
-        this.toggle(event)
-      }
-
-      // buttons
-
-      // [1] href
-      document.querySelector('[href="#collapseExample"]').addEventListener('click', this.clickTargetHandler)
-
-      // [2] data-target
-      document.querySelector('[data-target="#collapseExample"]').addEventListener('click', this.clickTargetHandler)      
+    getHeight() {
+      return this.options.element.getBoundingClientRect(this.options.element).height
     }
 
     toggle() {
-      document.querySelector('#collapseExample').classList.add('collapsing')
+      if (this.options.element.classList.contains('show')) {
+        return this.hide()
+      }
 
-      setTimeout(() => {
-        document.querySelector('#collapseExample').classList.remove('collapsing')
-        document.querySelector('#collapseExample').classList.toggle('show')
-      }, 600)
+      return this.show()
     }
 
-    static get version() {
-      return `${NAME}.${VERSION}`
+    show() {
+      if (this.onTransition) {
+        return false
+      }
+
+      if (this.options.element.classList.contains('show')) {
+        return false
+      }
+
+      this.onTransition = true
+
+      const onCollapsed = () => {
+        this.options.element.classList.add('show')
+        this.options.element.classList.remove('collapsing')
+        this.options.element.removeEventListener(Event.TRANSITION_END, onCollapsed)
+
+        this.options.element.setAttribute('aria-expanded', true)
+
+        this.onTransition = false
+      }
+
+      if (!this.options.element.classList.contains('collapsing')) {
+        this.options.element.classList.add('collapsing')
+      }
+
+      this.options.element.addEventListener(Event.TRANSITION_END, onCollapsed)
+
+      const height = this.getHeight()
+
+      this.options.element.style.height = '0px'
+
+      setTimeout(() => {
+        this.options.element.style.height = `${height}px`
+      }, 20)
+
+      return true
+    }
+
+    hide() {
+      if (this.onTransition) {
+        return false
+      }
+
+      if (!this.options.element.classList.contains('show')) {
+        return false
+      }
+
+      this.onTransition = true
+
+      const onCollapsed = () => {
+        this.options.element.classList.remove('collapsing')
+        this.options.element.style.height = 'auto'
+        this.options.element.removeEventListener(Event.TRANSITION_END, onCollapsed)
+
+        this.options.element.setAttribute('aria-expanded', false)
+
+        this.onTransition = false
+      }
+
+      this.options.element.style.height = '0px'
+
+      if (!this.options.element.classList.contains('collapsing')) {
+        this.options.element.classList.add('collapsing')
+      }
+
+      this.options.element.addEventListener(Event.TRANSITION_END, onCollapsed)
+
+      this.options.element.classList.remove('show')
+
+      return true
     }
 
     static _DOMInterface(options) {
-      return new Collapse(options)
+      return super._DOMInterface(Collapse, options)
     }
+  }
+
+  /**
+   * ------------------------------------------------------------------------
+   * DOM Api implementation
+   * ------------------------------------------------------------------------
+   */
+  const components = []
+
+  const collapses = document.querySelectorAll(`.${NAME}`)
+  if (collapses) {
+    collapses.forEach((element) => {
+      // const config = {}
+      const config = getAttributesConfig(element, DEFAULT_PROPERTIES, DATA_ATTRS_PROPERTIES)
+      config.element = element
+
+      components.push(Collapse._DOMInterface(config))
+    })
+  }
+
+  if (collapses) {
+    document.addEventListener('click', (event) => {
+      const target = findTargetByAttr(event.target, 'data-toggle')
+      if (!target) {
+        return
+      }
+
+      const dataToggleAttr = target.getAttribute('data-toggle')
+
+      if (dataToggleAttr && dataToggleAttr === NAME) {
+        let id = target.getAttribute('data-target') || target.getAttribute('href')
+        id = id.replace('#', '')
+
+        const component = components.find(c => c.getElement().getAttribute('id') === id)
+
+        if (!component) {
+          return
+        }
+
+        component.toggle()
+      }
+    })
   }
 
   return Collapse
