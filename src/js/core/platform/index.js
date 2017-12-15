341c3457638ade9ab446e6db59b101d8731fff07
Qathom <tony9295@gmail.com>
2017-12-15 02:44:09 +0100

    Remove extra features

--- src/js/core/platform/index.js
+++
@@ -1,8 +0,0 @@
-/*
- * Use of platform.js
- * https://github.com/bestiejs/platform.js
- * License: https://github.com/bestiejs/platform.js/blob/master/LICENSE
- */
-import platform from 'platform'
-
-export default platform
