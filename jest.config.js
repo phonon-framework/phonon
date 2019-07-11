938407687f16f5f7f2c2bd86d890f856cb9eb427
Qathom <tony9295@gmail.com>
2019-07-11 23:10:15 +0200

    Jest setup

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  verbose: true,
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)?$': 'babel-jest',
+  },
+  moduleDirectories: ['node_modules', 'src'],
+  globals: {
+    'ts-jest': {
+      babelConfig: true,
+    },
+  },
+};
